fd6c201c0d139949510336e9b98e010364f7481a
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-10-06 09:33:00 +0200

    Basic component added

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+const Mustache = require('mustache');
+
+export default class ReactMustache extends React.Component {
+
+  compileTemplate(template, data){
+    // lazy template compiling
+    return Mustache.render(template,data);
+  }
+
+  render() {
+    const { template, data, Component, ...props } = this.props
+    if (!template) return false
+
+    const __html = this.compileTemplate(template, data);
+
+    return (
+      <Component dangerouslySetInnerHTML={{__html}} {...props}/>
+    )
+  }
+}
+
+ReactMustache.defaultProps = {
+  data: {},
+  Component: 'div'
+}
